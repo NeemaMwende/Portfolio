3e0d29e6502edd7adb50f80874cff4ba5a995240
NeemaMwende <gracepete12@gmail.com>
2024-05-23 16:35:53 +0300

    Skills

---
+++ portfolio/src/utils/data.js
@@ -0,0 +1,40 @@
+export const SKILLS = [
+    {
+        title: "Frontend",
+        icon: "",
+        skills: [
+            { skill: "HTML5", percentage: "80%"},
+            { skill: "CSS3 (Sass/Scss)", percentage: "98%"},
+            { skill: "Javascript", percentage: "75%"},
+            { skill: "React.js", percentage: "88%"}
+        ],
+    },
+    {
+        title: "Backend",
+        icon: "",
+        skills: [
+            { skill: "Node.js", percentage: "70%"},
+            { skill: "Express.js", percentage: "65%"},
+        ],
+    },
+    {
+        title: "Tools",
+        icon: "",
+        skills: [
+            { skill: "Git & Github", percentage: "85%"},
+            { skill: "Visual Studio Code", percentage: "75%"},
+            { skill: "Webpack", percentage: "60%"},
+            { skill: "Responsive Design", percentage: "88%"}
+        ],
+    },
+    {
+        title: "Soft Skills",
+        icon: "",
+        skills: [
+            { skill: "Problem-solving", percentage: "80%"},
+            { skill: "Collaboration", percentage: "98%"},
+            { skill: "Attention to Detail", percentage: "75%"},
+            { skill: "Team-work", percentage: "90%"}
+        ],
+    },
+];
