ba20831f7e8abc96ecf65f5c18dfe820eb197163
NeemaMwende <gracepete12@gmail.com>
2024-05-24 18:58:43 +0300

    work experience components

--- portfolio/src/components/WorkExperience.jsx
+++ portfolio/src/components/WorkExperience.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import "./WorkExperience.css";
+import ExperienceCard from './ExperienceCard';
+import { WORK_EXPERIENCE } from "../../src/utils/data"
 
 const WorkExperience = () => {
   return (
-    <div>WorkExperience</div>
+    <section className='experience-container'>
+        <h5>Work Experience</h5>
+
+        <div className="experience-content">
+            {WORK_EXPERIENCE.map((item) => (
+              <ExperienceCard key={item.title} details={item}/>
+            ))}
+        </div>
+    </section>
   )
 }
 
-export default WorkExperience
+export default WorkExperience;
