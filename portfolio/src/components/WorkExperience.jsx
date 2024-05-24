551ec19612e8a9e17044775a8cc84fcf258f5cb6
NeemaMwende <gracepete12@gmail.com>
2024-05-24 19:11:57 +0300

    updates

--- portfolio/src/components/WorkExperience.jsx
+++ portfolio/src/components/WorkExperience.jsx
@@ -1,17 +1,40 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import "./WorkExperience.css";
 import ExperienceCard from './ExperienceCard';
-import { WORK_EXPERIENCE } from "../../src/utils/data"
+import { WORK_EXPERIENCE } from "../../src/utils/data";
+import Slider from "react-slick";
 
 const WorkExperience = () => {
+  const sliderRef = useRef();
+
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    arrows: false,
+    responsive: [
+      {
+        breakpoint: 769,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
+
   return (
     <section className='experience-container'>
         <h5>Work Experience</h5>
 
         <div className="experience-content">
+          <Slider ref={sliderRef} {...settings}>
             {WORK_EXPERIENCE.map((item) => (
               <ExperienceCard key={item.title} details={item}/>
             ))}
+          </Slider>
         </div>
     </section>
   )
