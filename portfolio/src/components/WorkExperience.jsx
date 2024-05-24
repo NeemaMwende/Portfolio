6e16df7b8122a361c46dc06b88945ba6124594d7
NeemaMwende <gracepete12@gmail.com>
2024-05-24 22:33:59 +0300

    updates

--- portfolio/src/components/WorkExperience.jsx
+++ portfolio/src/components/WorkExperience.jsx
@@ -1,8 +1,68 @@
+// import React, { useRef } from 'react';
+// import "./WorkExperience.css";
+// import ExperienceCard from './ExperienceCard';
+// import { WORK_EXPERIENCE } from "../../src/utils/data";
+// import Slider from "react-slick";
+
+// const WorkExperience = () => {
+//   const sliderRef = useRef();
+
+//   const settings = {
+//     dots: false,
+//     infinite: true,
+//     speed: 500,
+//     slidesToShow: 2,
+//     slidesToScroll: 1,
+//     arrows: false,
+//     responsive: [
+//       {
+//         breakpoint: 769,
+//         settings: {
+//           slidesToShow: 1,
+//           slidesToScroll: 1,
+//         },
+//       },
+//     ],
+//   };
+
+//   const slideRight = () => {
+//     sliderRef.current.slickNext();
+//   };
+//   const slideLeft = () => {
+//     sliderRef.current.slickPrev();
+//   };
+
+//   return (
+//     <section className='experience-container'>
+//         <h5>Work Experience</h5>
+
+//         <div className="experience-content">
+//           <div className="arrow-right" onClick={slideRight}>
+//             <span className="material-symbols-outlined">chevron_right</span>
+//           </div>
+
+//           <div className="arrow-left" onClick={slideLeft}>
+//             <span className="material-symbols-outlined">chevron_left</span>
+//           </div>
+
+//           <Slider ref={sliderRef} {...settings}>
+//             {WORK_EXPERIENCE.map((item) => (
+//               <ExperienceCard key={item.title} details={item}/>
+//             ))}
+//           </Slider>
+//         </div>
+//     </section>
+//   )
+// }
+
+// export default WorkExperience;
 import React, { useRef } from 'react';
 import "./WorkExperience.css";
 import ExperienceCard from './ExperienceCard';
 import { WORK_EXPERIENCE } from "../../src/utils/data";
 import Slider from "react-slick";
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
 
 const WorkExperience = () => {
   const sliderRef = useRef();
@@ -28,31 +88,29 @@ const WorkExperience = () => {
   const slideRight = () => {
     sliderRef.current.slickNext();
   };
+
   const slideLeft = () => {
     sliderRef.current.slickPrev();
   };
 
   return (
     <section className='experience-container'>
-        <h5>Work Experience</h5>
-
-        <div className="experience-content">
-          <div className="arrow-right" onClick={slideRight}>
-            <span className="material-symbols-outlined">chevron_right</span>
-          </div>
-
-          <div className="arrow-left" onClick={slideLeft}>
-            <span className="material-symbols-outlined">chevron_left</span>
-          </div>
-
-          <Slider ref={sliderRef} {...settings}>
-            {WORK_EXPERIENCE.map((item) => (
-              <ExperienceCard key={item.title} details={item}/>
-            ))}
-          </Slider>
+      <h5>Work Experience</h5>
+      <div className="experience-content">
+        <div className="arrow-right" onClick={slideRight}>
+          <span className="material-symbols-outlined">chevron_right</span>
         </div>
+        <div className="arrow-left" onClick={slideLeft}>
+          <span className="material-symbols-outlined">chevron_left</span>
+        </div>
+        <Slider ref={sliderRef} {...settings}>
+          {WORK_EXPERIENCE.map((item) => (
+            <ExperienceCard key={item.title} details={item} />
+          ))}
+        </Slider>
+      </div>
     </section>
-  )
+  );
 }
 
 export default WorkExperience;
