81eb80e17ce7c31ed51a8c0dca20cc3a87e218dd
NeemaMwende <gracepete12@gmail.com>
2024-05-25 22:13:28 +0300

    updates

--- portfolio/src/components/ContactMe.jsx
+++ portfolio/src/components/ContactMe.jsx
@@ -9,33 +9,41 @@ import ContactForm from './ContactForm';
 
 const ContactMe = () => {
   return (
-   <section className='contact-container' id='contact'>
+    <section className='contact-container' id='contact'>
       <h5>Contact Me</h5>
 
       <div className="contact-content">
         <div style={{ flex: 1 }}>
-          <ContactMeCard
-            iconUrl={email}
-            text="neemamwende009@gmail.com"
-          />
-          <ContactMeCard
-            iconUrl={github}
-            text="https://github.com/NeemaMwende"
-          />
-           <ContactMeCard
-            iconUrl={linkedIn}
-            text="https://www.linkedin.com/in/neema-mwende-2089a8273"
-          />
-           <ContactMeCard
-            iconUrl={twitter}
-            text="https://x.com/GracePeter1212"
-          />
+          <a href="mailto:neemamwende009@gmail.com" target="_blank" rel="noopener noreferrer">
+            <ContactMeCard
+              iconUrl={email}
+              text="neemamwende009@gmail.com"
+            />
+          </a>
+          <a href="https://github.com/NeemaMwende" target="_blank" rel="noopener noreferrer">
+            <ContactMeCard
+              iconUrl={github}
+              text="https://github.com/NeemaMwende"
+            />
+          </a>
+          <a href="https://www.linkedin.com/in/neema-mwende-2089a8273" target="_blank" rel="noopener noreferrer">
+            <ContactMeCard
+              iconUrl={linkedIn}
+              text="https://www.linkedin.com/in/neema-mwende-2089a8273"
+            />
+          </a>
+          <a href="https://x.com/GracePeter1212" target="_blank" rel="noopener noreferrer">
+            <ContactMeCard
+              iconUrl={twitter}
+              text="https://x.com/GracePeter1212"
+            />
+          </a>
         </div>
         <div style={{ flex: 1 }}>
           <ContactForm />
         </div>
       </div>
-   </section>
+    </section>
   );
 };
 
